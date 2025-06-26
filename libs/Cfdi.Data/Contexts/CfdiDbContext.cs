83c211c0e1f345b852fc37f766e86d9225848c72
Julio Cesar Bautista Monsalvo <desarrollohg@hgtransportaciones.com>
2025-06-26 09:54:58 -0600

    Se elimina conecction por default en context

--- libs/Cfdi.Data/Contexts/CfdiDbContext.cs
+++ libs/Cfdi.Data/Contexts/CfdiDbContext.cs
@@ -104,10 +104,6 @@ public partial class CfdiDbContext : DbContext
 
     public virtual DbSet<vwTipoCambio> vwTipoCambios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=hg.sql.midireccion.com;User ID=uAppCFDis;Password=U1nv01c3$.; Database=CFDisDB;TrustServerCertificate=true;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AggregatedCounter>(entity =>
